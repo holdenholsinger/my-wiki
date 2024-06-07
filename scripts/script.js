32812e96d014e26f63e491cfe809e462ad453539
holho <holholsinger@gmail.com>
2024-06-07 16:06:13 -0400

    added js and hamburger

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
-document.getElementById('hamburger').addEventListener('click', function() {
+function toggleNavLinks() {
+    console.log('Hamburger clicked')
     // Toggle the 'show' class only on the links other than 'Home' and 'Hamburger'
     document.querySelectorAll('.navbar a:not(#home-button):not(#hamburger)').forEach(function(link) {
         link.classList.toggle('show');
     });
-});
+}
+
+document.getElementById('hamburger').addEventListener('click', toggleNavLinks);
